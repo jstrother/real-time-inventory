edc24539f308fe00135568a39671ecff71cfa6b1
Jim Strother <strotherwebdev@gmail.com>
2017-01-04 03:40:00 +0000

    starting to get server.js up and running; next is get rethink going

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './components/index.jsx',
+	output: {
+		path: `${__dirname}/public`,
+		filename: 'scripts.js'
+	},
+	watch: true,
+	devtool: 'source-map',
+	module: {
+		loaders: [{
+			test: /.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				presets: [
+					'latest',
+					'react'
+				],
+				plugins: [
+					'transform-class-properties'
+				]
+			}
+		}]
+	}
+};
