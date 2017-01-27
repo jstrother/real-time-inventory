4ebea0ede5c16acd0144a54ab62e4edc16e57381
Jim Strother <jim.strother@gmail.com>
2017-01-26 20:54:07 -0800

    changed PORT variable in server.js from 8080 to 9000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const r = require('rethinkdb');
 const changefeedSocketEvents = require('./socket-events.js');
-const PORT = 8080;
+const PORT = 9000;
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
