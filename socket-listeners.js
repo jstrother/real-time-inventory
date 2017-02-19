4b65c3a58d06656fb8e5476720703e3eae17e3d0
Jim Strother <jim.strother@gmail.com>
2017-02-18 16:40:42 -0800

    coming back to refactor for mobile- oops

---
+++ socket-listeners.js
@@ -0,0 +1,28 @@
+// ./socket-listeners/inventory-listener.js
+// imported into index.jsx
+
+import io from 'socket.io-client';
+const socket = io.connect('/');
+
+export const InventorySocketListener = (store) => {
+    socket.on('item:insert', (item) => {
+        store.dispatch({
+            type: 'item:insert',
+            item
+        });
+    });
+    
+    socket.on('item:update', (item) => {
+        store.dispatch({
+            type: 'item:update',
+            item
+        });
+    });
+    
+    socket.on('item:delete', (item) => {
+        store.dispatch({
+            type: 'item:delete',
+            item
+        });
+    });
+};
